572c06a3919d7a788f6822580d53e538da41f067
ngkong <ngkongs@gmail.com>
2020-11-02 15:47:32 +0700

    adjust test 1

--- test/1_TadUniswapMiningBasicFunctions.js
+++ test/1_TadUniswapMiningBasicFunctions.js
@@ -1,98 +1,98 @@
 var BN = web3.utils.BN;
 
-const TenTokenTest = artifacts.require('TenTokenTest');
+const LPTokenTest = artifacts.require('LPTokenTest');
 const TadTokenTest = artifacts.require('TadTokenTest');
-const TadGenesisMining = artifacts.require('TadGenesisMining');
+const TadUniswapMining = artifacts.require('TadUniswapMining');
 
 var tadTokenTestInstance;
-var tenTokenTestInstance;
+var lpTokenTestInstance;
 
-contract('TadGenesisMining', (accounts) => {
+contract('TadUniswapMining', (accounts) => {
 
 
-    it('should mint 1000000 TAD to Genesis Contract', async () => {
+    it('should mint 1000000 TAD to Mining Contract', async () => {
         tadTokenTestInstance = await TadTokenTest.deployed();
 
         //block 1
-        await tadTokenTestInstance.mint(TadGenesisMining.address, "100000000000000000000000000", { from: accounts[0] });
+        await tadTokenTestInstance.mint(TadUniswapMining.address, "100000000000000000000000000", { from: accounts[0] });
 
-        const balance = await tadTokenTestInstance.balanceOf(TadGenesisMining.address);
+        const balance = await tadTokenTestInstance.balanceOf(TadUniswapMining.address);
         assert.equal(balance, "100000000000000000000000000", 'The balance is not 1000000 TAD');
     });
 
-    it('should mint 100000 TEN to the first account', async () => {
-        tenTokenTestInstance = await TenTokenTest.deployed();
+    it('should mint 100000 LP Token to the first account', async () => {
+        lpTokenTestInstance = await LPTokenTest.deployed();
 
         //block 2
-        await tenTokenTestInstance.mint(accounts[0], "10000000000000000000000000", { from: accounts[0] });
+        await lpTokenTestInstance.mint(accounts[0], "10000000000000000000000000", { from: accounts[0] });
 
-        const balance = await tenTokenTestInstance.balanceOf(accounts[0]);
-        assert.equal(balance, "10000000000000000000000000", 'The balance is not 100000 TEN');
+        const balance = await lpTokenTestInstance.balanceOf(accounts[0]);
+        assert.equal(balance, "10000000000000000000000000", 'The balance is not 100000 LP Token');
     });
 
-    it('should approve 5000000 TEN to Genesis address', async () => {
-        tenTokenTestInstance = await TenTokenTest.deployed();
+    it('should approve 5000000 LP Token to Mining address', async () => {
+        lpTokenTestInstance = await LPTokenTest.deployed();
 
         //block 3
-        await tenTokenTestInstance.approve(TadGenesisMining.address, "500000000000000000000000000", { from: accounts[0] });
+        await lpTokenTestInstance.approve(TadUniswapMining.address, "500000000000000000000000000", { from: accounts[0] });
 
-        const allowance = await tenTokenTestInstance.allowance(accounts[0], TadGenesisMining.address);
-        assert.equal(allowance, "500000000000000000000000000", 'The allowance is not 5000000 TEN');
+        const allowance = await lpTokenTestInstance.allowance(accounts[0], TadUniswapMining.address);
+        assert.equal(allowance, "500000000000000000000000000", 'The allowance is not 5000000 LP Token');
     });
 
-    it('should stake 1000 TEN', async () => {
+    it('should stake 1000 LP Token', async () => {
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
         
         //block 4
-        await genesisInstance.stake("1000000000000000000000", { from: accounts[0] });
+        await miningInstance.stake("1000000000000000000000", { from: accounts[0] });
 
-        const stake = await genesisInstance.stakeHolders(accounts[0]);
-        assert.equal(stake, "1000000000000000000000", 'The stake is not 1000 TEN');
+        const stake = await miningInstance.stakeHolders(accounts[0]);
+        assert.equal(stake, "1000000000000000000000", 'The stake is not 1000 LP Token');
 
     });
 
     //after 5 blocks should claim 5*1.15 TAD
     it('should claim 5.75 TAD', async () =>{
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
         //block 5
-        await genesisInstance.claimTad({ from: accounts[0] });
+        await miningInstance.claimTad({ from: accounts[0] });
         balance = await tadTokenTestInstance.balanceOf(accounts[0]);
-        assert.equal(balance.toString(), "5750000000000000000", 'Didnt claim 5.75 TEN');
+        assert.equal(balance.toString(), "5750000000000000000", 'Didnt claim 5.75 LP Token');
     });
 
     it('should claim 115 TAD after 100 blocks', async () =>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
         
         //block 6
-        await tenTokenTestInstance.mint(accounts[1], "10000000000000000000000000", { from: accounts[0] });
+        await lpTokenTestInstance.mint(accounts[1], "10000000000000000000000000", { from: accounts[0] });
 
         for(i=0; i<93; i++){ //block 7-99
-            await genesisInstance.doNothing({ from: accounts[0] });
+            await miningInstance.doNothing({ from: accounts[0] });
         }
 
         //block 100
-        await genesisInstance.claimTad({ from: accounts[0] });
+        await miningInstance.claimTad({ from: accounts[0] });
         balance = await tadTokenTestInstance.balanceOf(accounts[0]);
 
         assert.equal(balance, "115000000000000000000", 'Didnt claim 115 TAD');
     });
 
-    it('should claim 0.23 TAD for account1 after stake 250 TEN', async () =>{
+    it('should claim 0.23 TAD for account1 after stake 250 LP Token', async () =>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
         //block 101
-        await tenTokenTestInstance.approve(TadGenesisMining.address, "500000000000000000000000000", { from: accounts[1] });
+        await lpTokenTestInstance.approve(TadUniswapMining.address, "500000000000000000000000000", { from: accounts[1] });
         
-        //stake 250 TEN for account 1
+        //stake 250 LP Token for account 1
         //block 102
-        await genesisInstance.stake("250000000000000000000", { from: accounts[1] }); 
+        await miningInstance.stake("250000000000000000000", { from: accounts[1] }); 
 
         //block 103
-        await genesisInstance.claimTad({ from: accounts[1] });
+        await miningInstance.claimTad({ from: accounts[1] });
         
         balance = await tadTokenTestInstance.balanceOf(accounts[1]);
 
@@ -102,14 +102,14 @@ contract('TadGenesisMining', (accounts) => {
 
     it('should claim more 1.15 TAD for account1 after 5 blocks', async () =>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
         for(i=0; i<4; i++){ //block 104-107
-            await genesisInstance.doNothing({ from: accounts[1] });
+            await miningInstance.doNothing({ from: accounts[1] });
         }
 
         //block 108
-        await genesisInstance.claimTad({ from: accounts[1] });
+        await miningInstance.claimTad({ from: accounts[1] });
         
         balance = await tadTokenTestInstance.balanceOf(accounts[1]);
 
@@ -120,10 +120,10 @@ contract('TadGenesisMining', (accounts) => {
 
     it('should have 123.74 TAD for account0', async () =>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
         //block 109
-        await genesisInstance.claimTad({ from: accounts[0] });
+        await miningInstance.claimTad({ from: accounts[0] });
         
         balance = await tadTokenTestInstance.balanceOf(accounts[0]);
 
@@ -131,45 +131,45 @@ contract('TadGenesisMining', (accounts) => {
         assert.equal(balance.toString(), "123740000000000000000", 'Did not claim 123.74 TAD');
     });
 
-    it('should accept stake of 1250 TEN from account2', async() => {
+    it('should accept stake of 1250 LP Token from account2', async() => {
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
         //block 110
-        await tenTokenTestInstance.mint(accounts[2], "100000000000000000000000", { from: accounts[0] });
+        await lpTokenTestInstance.mint(accounts[2], "100000000000000000000000", { from: accounts[0] });
 
         //block 111
-        await tenTokenTestInstance.approve(TadGenesisMining.address, "500000000000000000000000000", { from: accounts[2] });
+        await lpTokenTestInstance.approve(TadUniswapMining.address, "500000000000000000000000000", { from: accounts[2] });
         
         //block 112
-        await genesisInstance.stake("1250000000000000000000", { from: accounts[2] }); 
+        await miningInstance.stake("1250000000000000000000", { from: accounts[2] }); 
 
-        stake = await genesisInstance.stakeHolders(accounts[2]);
-        assert.equal(stake, "1250000000000000000000", 'Stake account2 != 1250 TEN');
+        stake = await miningInstance.stakeHolders(accounts[2]);
+        assert.equal(stake, "1250000000000000000000", 'Stake account2 != 1250 LP Token');
 
     });
 
-    it('should be able to unstake 500 TEN from account2', async()=>{
-        genesisInstance = await TadGenesisMining.deployed();
+    it('should be able to unstake 500 LP Token from account2', async()=>{
+        miningInstance = await TadUniswapMining.deployed();
 
         //block 113
-        await genesisInstance.unstake("500000000000000000000", { from: accounts[2] }); 
+        await miningInstance.unstake("500000000000000000000", { from: accounts[2] }); 
 
-        stake = await genesisInstance.stakeHolders(accounts[2]);
-        assert.equal(stake, "750000000000000000000", 'Stake account2 != 750 TEN');
+        stake = await miningInstance.stakeHolders(accounts[2]);
+        assert.equal(stake, "750000000000000000000", 'Stake account2 != 750 LP Token');
     });
 
     it('should give correct claim balances after current block exceeds endMiningBlockNum', async()=>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
         
         for(i=0; i<100; i++){ //block 114-213
-            await genesisInstance.doNothing({ from: accounts[0] });
+            await miningInstance.doNothing({ from: accounts[0] });
         }
 
-        await genesisInstance.claimTad({ from: accounts[0] });
-        await genesisInstance.claimTad({ from: accounts[1] });
-        await genesisInstance.claimTad({ from: accounts[2] });
+        await miningInstance.claimTad({ from: accounts[0] });
+        await miningInstance.claimTad({ from: accounts[1] });
+        await miningInstance.claimTad({ from: accounts[2] });
 
         balance0 = await tadTokenTestInstance.balanceOf(accounts[0]);
         balance1 = await tadTokenTestInstance.balanceOf(accounts[1]);
@@ -186,37 +186,37 @@ contract('TadGenesisMining', (accounts) => {
 
     });
 
-    it("should return correct TEN value when unstake", async()=>{
+    it("should return correct LP Token value when unstake", async()=>{
 
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
-        balance0_before = await tenTokenTestInstance.balanceOf(accounts[0]);
-        balance1_before = await tenTokenTestInstance.balanceOf(accounts[1]);
-        balance2_before = await tenTokenTestInstance.balanceOf(accounts[2]);
+        balance0_before = await lpTokenTestInstance.balanceOf(accounts[0]);
+        balance1_before = await lpTokenTestInstance.balanceOf(accounts[1]);
+        balance2_before = await lpTokenTestInstance.balanceOf(accounts[2]);
 
-        await genesisInstance.unstake(0, { from: accounts[0] });
-        await genesisInstance.unstake(0, { from: accounts[1] });
-        await genesisInstance.unstake(0, { from: accounts[2] });
+        await miningInstance.unstake(0, { from: accounts[0] });
+        await miningInstance.unstake(0, { from: accounts[1] });
+        await miningInstance.unstake(0, { from: accounts[2] });
 
-        balance0_after = await tenTokenTestInstance.balanceOf(accounts[0]);
-        balance1_after = await tenTokenTestInstance.balanceOf(accounts[1]);
-        balance2_after = await tenTokenTestInstance.balanceOf(accounts[2]);
+        balance0_after = await lpTokenTestInstance.balanceOf(accounts[0]);
+        balance1_after = await lpTokenTestInstance.balanceOf(accounts[1]);
+        balance2_after = await lpTokenTestInstance.balanceOf(accounts[2]);
 
-        assert.equal(new BN(balance0_after).sub(new BN(balance0_before)), "1000000000000000000000", 'unstake account0 != 1000 TEN');
-        assert.equal(new BN(balance1_after).sub(new BN(balance1_before)), "250000000000000000000", 'unstake account0 != 250 TEN');
-        assert.equal(new BN(balance2_after).sub(new BN(balance2_before)), "750000000000000000000", 'unstake account0 != 1250 TEN');
+        assert.equal(new BN(balance0_after).sub(new BN(balance0_before)), "1000000000000000000000", 'unstake account0 != 1000 LP Token');
+        assert.equal(new BN(balance1_after).sub(new BN(balance1_before)), "250000000000000000000", 'unstake account0 != 250 LP Token');
+        assert.equal(new BN(balance2_after).sub(new BN(balance2_before)), "750000000000000000000", 'unstake account0 != 1250 LP Token');
 
     });
 
-    it("should hold 0 TEN", async()=>{
+    it("should hold 0 LP Token", async()=>{
         
-        genesisInstance = await TadGenesisMining.deployed();
+        miningInstance = await TadUniswapMining.deployed();
 
-        tenBalanceGenesis = await tenTokenTestInstance.balanceOf(TadGenesisMining.address);
-        totalStaked = await genesisInstance.totalStaked();
+        lpBalanceMining = await lpTokenTestInstance.balanceOf(TadUniswapMining.address);
+        totalStaked = await miningInstance.totalStaked();
 
 
-        assert.equal(tenBalanceGenesis, "0", 'ten balance in genesis contract is supposed to be 0');
+        assert.equal(lpBalanceMining, "0", 'LP Token balance in mining contract is supposed to be 0');
         assert.equal(totalStaked, "0", 'total stake is supposed to be 0');
         
     });
